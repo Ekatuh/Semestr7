2ea41306539b262efdef9ce8b9b736365953c09d
Екатерина Кузьменко <katya.kuzmen@yandex.ru>
2025-10-12 20:35:55 +0700

    Настройка пайплайна 3.0

--- test_discriminant.py
+++ test_discriminant.py
@@ -1,16 +1,15 @@
+import unittest
 import discriminant
 
+class TestDiscriminant(unittest.TestCase):
+    def test_positive_discriminant(self):
+        # Положительный тест: D > 0
+        self.assertEqual(discriminant.discriminant(1, 5, 6), 1)
 
-def test_positive_discriminant():
-    # Положительный тест: D > 0
-    assert discriminant.discriminant(1, 5, 6) == 1
+    def test_zero_discriminant(self):
+        # Положительный тест: D = 0
+        self.assertEqual(discriminant.discriminant(1, 2, 1), 0)
 
-
-def test_zero_discriminant():
-    # Положительный тест: D = 0
-    assert discriminant.discriminant(1, 2, 1) == 0
-
-
-def test_negative_discriminant():
-    # Негативный тест: D < 0
-    assert discriminant.discriminant(5, 2, 3) == -56
+    def test_negative_discriminant(self):
+        # Негативный тест: D < 0
+        self.assertEqual(discriminant.discriminant(5, 2, 3), -56)
