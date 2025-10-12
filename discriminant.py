1ec7adccf311f2c9af932ec6e2f3c29f0b508c52
Екатерина Кузьменко <katya.kuzmen@yandex.ru>
2025-10-12 20:28:27 +0700

    Настройка пайплайна 2.0

--- discriminant.py
+++ discriminant.py
@@ -1,5 +1,6 @@
 def discriminant(a, b, c):
-    return b**2 - 4*a*c
+    """Вычисляет дискриминант квадратного уравнения."""
+    return b**2 - 4 * a * c
 
 
 if __name__ == "__main__":
--- test_discriminant.py
+++ test_discriminant.py
@@ -1,13 +1,16 @@
 import discriminant
 
+
 def test_positive_discriminant():
-    #Положительный тест: D > 0
+    # Положительный тест: D > 0
     assert discriminant.discriminant(1, 5, 6) == 1
 
+
 def test_zero_discriminant():
-    #Положительный тест: D = 0
+    # Положительный тест: D = 0
     assert discriminant.discriminant(1, 2, 1) == 0
 
+
 def test_negative_discriminant():
-    #Негативный тест: D < 0
+    # Негативный тест: D < 0
     assert discriminant.discriminant(5, 2, 3) == -56
